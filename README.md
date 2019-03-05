8a66b24653ba73b6d5973825ece2dc8a8fd19ecc
blitznihar <blitznihar@gmail.com>
2019-03-04 22:30:53 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# terransible-infra-docker-swarm-aws
+Docker Swarm 3 node cluster on AWS
---
+++ ansible/roles/dockerswarmminion/files/joincommand.sh
@@ -0,0 +1,6 @@
+#To add a worker to this swarm, run the following command:
+
+    docker swarm join \
+    --token SWMTKN-1-1vw4uu8e8g0o47hun47nwbe8j66ui0h8t9lleprxkrkgm910rb-73elxz7ee1s62c3wkrk9xtg9g \
+    10.0.1.43:2377
+
